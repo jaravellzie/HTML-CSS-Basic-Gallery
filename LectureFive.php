fb05a51d2c4ab54e9fa3f55d90fa59b399f4f1b1
jaravellzie <53119278+jaravellzie@users.noreply.github.com>
2019-09-16 11:02:09 +0800

    Add files via upload

---
+++ LectureFive.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+	<title>CSS with Pictures</title>
+	<link rel="stylesheet" href="index-style-1.css"/>
+</head>
+<body>
+
+	<div id="div1">
+		<center><a href="LectureFive.php"><label class="lblClass">Jamarah Smiling!</label></a></center>
+		<img class="image" src="JamarahOne.jpg"/>
+	</div>
+
+	<div id="div2">
+		<center><a href="LectureFive.php"><label class="lblClass">Jamarah Planting!</label></a></center>
+		<img class="image" src="JamarahTwo.jpg"/>
+	</div>
+
+	<div id="div3">
+		<center><a href="LectureFive.php"><label class="lblClass">Jamarah Surfing!</label></a></center>
+		<img class="image" src="JamarahThree.jpg"/>
+	</div>
+
+	<div id="div4">
+		<center><a href="LectureFive.php"><label class="lblClass">Jamarah Smiling!</label></a></center>
+		<img class="image" src="JamarahFour.jpg"/>
+	</div>
+
+</body>
+</html>
